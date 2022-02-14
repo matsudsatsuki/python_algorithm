3fce0feeff540c8e472d864d44ca774be1a76219
matsudasatsuki <yk.xxx.org@gmail.com>
2022-02-14 16:39:02 +0900

    add celeb_problem

---
+++ celeb_problem.py
@@ -0,0 +1,37 @@
+def naive_celeb(G):
+    n = len(G)
+    for u in range(n):
+        for v in range(n):
+            if u == v:
+                continue
+            if G[u][v]:
+                break
+            if not G[v][u]:
+                break
+        else:
+            return u
+    return None
+
+def celeb(G):
+    n = len(G)
+    u,v = 0,1
+    for c in range(2,n+1):
+        if G[u][v]:
+            u = c
+        else:
+            v = c
+    if u == n:
+        c = v
+    else:
+        c = u
+    for v in range(n):
+        if c == v:
+            continue
+        if G[c][v]:
+            break
+        if not G[v][c]:
+            break
+    else:
+        return c
+    return None
+#セレブ問題で探しているのは、他の全てのノードからエッジが入っているが、そのノードからエッジが出ていかないノード
