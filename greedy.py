605b11c641ec4a97eb5a65fcdc56a37caaf5f31e
matsudasatsuki <yk.xxx.org@gmail.com>
2022-03-04 16:09:58 +0900

    add kruscal,prim

--- greedy.py
+++ greedy.py
@@ -24,3 +24,64 @@ def codes(tree,prefix=''):
     for bit,child in zip('01',tree):
         for pair in codes(child,prefix+bit):
             yield pair
+#Kruskalのアルゴリズムの簡単な実装
+def naive_find(C,u):
+    while C[u] != u:
+        u = C[u]
+    return u
+
+def naive_union(C,u,v):
+    u = naive_find(C,u)
+    v = naive_find(C,v)
+    C[u] = v
+
+def naive_kruskal(G):
+    E = [(G[u][v],u,v) for u in G for v in G[u]]
+    T = set()
+    C = {u:u for u in G }
+    for _, u, v in sorted(E):
+        if naive_find(C,u) != naive_find(C,v):
+            T.add((u,v))
+            naive_union((C,u,v))
+    return T
+
+#kruskalのアルゴリズム
+def find(C,u):
+    if C[u] != u:
+        C[u] = find(C,C[u])#パス圧縮
+    return C[u]
+
+def union(C,R,u,v):
+    u,v = find(C,u),find(C,v)
+    if R[u] > R[v]:#ランクによる統合
+        C[v] = u
+    else:
+        C[u] = v
+    if R[u] == R[v]:
+        R[v] += 1#同ランクの場合,vを一つ上にあげる
+
+def kruskal(G):
+    E = [(G[u][v],u,v)for u in G for v in G[u]]
+    T = set()
+    C,R = {u:u for u in G},{u:0 for u in G}#成分の代表とランク
+    for _,u,v in sorted(E):
+        if find(C,u) != find(C,v):
+            T.add((u,v))
+            union(C,R,u,v)
+    return T
+
+#Primのアルゴリズム
+from heapq import heappop,heappush
+
+def prim(G,s):
+    P,Q = {},[(0,None,s)]
+    while Q:
+        _,p,u = heappop(Q)
+        if u in P:
+            continue
+        P[u] = p
+        for v,w in G[u].items():
+            heappush(Q,(w,u,v))
+    return P
+    
+
